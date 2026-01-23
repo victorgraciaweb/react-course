058befe6e6e206a5664dadac6ca6379378f40fa5
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-01-23 12:09:42 -0300

    Added useEffect logic

---
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithEffect.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+
+const colors = {
+  red: 'bg-red-500 animate-pulse',
+  yellow: 'bg-yellow-500 animate-pulse',
+  green: 'bg-green-500 animate-pulse',
+};
+
+type trafficLightColor = keyof typeof colors;
+
+export const TrafficLightWithEffect = () => {
+  const [ligtht, setLight] = useState<trafficLightColor>('red');
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    if (countdown === 0) return;
+
+    const intervalId = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [countdown]);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-white text-3xl font-thin">
+          Semáforo con useEffect
+        </h1>
+        <h2 className="text-white text-xl">Countdown: {countdown}</h2>
+        <div
+          className={`w-32 h-32 ${
+            ligtht === 'red' ? colors[ligtht] : 'bg-gray-500'
+          } rounded-full`}
+        ></div>
+
+        <div
+          className={`w-32 h-32 ${
+            ligtht === 'yellow' ? colors[ligtht] : 'bg-gray-500'
+          } rounded-full`}
+        ></div>
+
+        <div
+          className={`w-32 h-32 ${
+            ligtht === 'green' ? colors[ligtht] : 'bg-gray-500'
+          } rounded-full`}
+        ></div>
+      </div>
+    </div>
+  );
+};
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-//import { HooksApp } from './HooksApp';
-import { TrafficLight } from './01-useState/TrafficLight';
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficLight />
+    <TrafficLightWithEffect />
   </StrictMode>,
 );
