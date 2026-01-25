26841ee98025b0426af5534fc174affc985f9c12
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-01-25 12:36:34 -0300

    Set shadcn

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { FocusScreen } from './04-useRef/FocusScreen';
+import { TasksApp } from './05-useReducer/TaskApp';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FocusScreen />
+    <TasksApp />
   </StrictMode>,
 );
