fa22d8f5184f608232390629f2ae9b017ab716ae
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-02-11 16:15:42 -0300

    Added Hook useTransition

--- 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
+++ 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,4 @@
-import { useOptimistic, useState } from 'react';
+import { useOptimistic, useState, useTransition } from 'react';
 
 interface Comment {
   id: number;
@@ -6,7 +6,11 @@ interface Comment {
   optimistic?: boolean;
 }
 
+let lastId = 2;
+
 export const InstagromApp = () => {
+  const [isPending, startTransition] = useTransition();
+
   const [comments, setComments] = useState<Comment[]>([
     { id: 1, text: '¡Gran foto!' },
     { id: 2, text: 'Me encanta 🧡' },
@@ -15,10 +19,11 @@ export const InstagromApp = () => {
   const [optimisticComments, addOptimisticComment] = useOptimistic(
     comments,
     (currentComments, newCommentText: string) => {
+      lastId++;
       return [
         ...currentComments,
         {
-          id: new Date().getTime(),
+          id: lastId,
           text: newCommentText,
           optimistic: true,
         },
@@ -31,13 +36,15 @@ export const InstagromApp = () => {
 
     addOptimisticComment(messageText);
 
-    // Simule HTTP request
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    startTransition(async () => {
+      // Simule HTTP request
+      await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    setComments((prev) => [
-      ...prev,
-      { id: new Date().getTime(), text: messageText },
-    ]);
+      setComments((prev) => [
+        ...prev,
+        { id: new Date().getTime(), text: messageText },
+      ]);
+    });
   };
 
   return (
@@ -83,7 +90,7 @@ export const InstagromApp = () => {
         />
         <button
           type="submit"
-          disabled={false}
+          disabled={isPending}
           className="bg-blue-500 text-white p-2 rounded-md w-full"
         >
           Enviar
