0ffb571b2c969274aad9bde76fec6dbda528de95
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-02-21 09:11:57 -0300

    Added menu

---
+++ 05-heroes-app/src/components/custom/CustomMenu.tsx
@@ -0,0 +1,44 @@
+import {
+  NavigationMenuItem,
+  NavigationMenuLink,
+} from '@radix-ui/react-navigation-menu';
+import { Link, useLocation } from 'react-router';
+import { NavigationMenu, NavigationMenuList } from '../ui/navigation-menu';
+import { cn } from '@/lib/utils';
+
+export const CustomMenu = () => {
+  const { pathname } = useLocation();
+
+  const isActive = (path: string) => {
+    return pathname === path;
+  };
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        {/* Home */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(isActive('/') && 'bg-slate-200', 'p-2 rounded-md')}
+          >
+            <Link to="/">Inicio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        {/* Search */}
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            asChild
+            className={cn(
+              isActive('/search') && 'bg-slate-200',
+              'p-2 rounded-md',
+            )}
+          >
+            <Link to="/search">Buscar superhéroes</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
