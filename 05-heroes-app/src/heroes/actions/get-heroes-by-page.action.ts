6acb13b356aed6f05ff21d8d20ed7bec6ba9eb00
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-02-23 13:31:29 -0300

    Typed HeroesResponse

--- 05-heroes-app/src/heroes/actions/get-heroes-by-page.action.ts
+++ 05-heroes-app/src/heroes/actions/get-heroes-by-page.action.ts
@@ -1,9 +1,18 @@
 import { heroApi } from '../api/hero.api';
+import type { HeroesResponse } from '../types/get-heroes.response';
 
-export const getHeroesByPageAction = async () => {
-  const { data } = await heroApi.get(`/`);
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-  console.log({ data });
+export const getHeroesByPageAction = async (): Promise<HeroesResponse> => {
+  const { data } = await heroApi.get<HeroesResponse>(`/`);
 
-  return data;
+  const heroes = data.heroes.map((hero) => ({
+    ...hero,
+    image: `${BASE_URL}/images/${hero.image}`,
+  }));
+
+  return {
+    ...data,
+    heroes: heroes,
+  };
 };
