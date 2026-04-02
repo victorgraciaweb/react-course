7a6599d131a7fed0f9fc799932769d49b94fdc4d
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-02 03:00:18 +0200

    Set testing library and vitest

---
+++ 05-heroes-app/src/main.test.tsx
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Main test', () => {
+  test('Should return true', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- 05-heroes-app/vite.config.ts
+++ 05-heroes-app/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
   },
 });
