5c6e2c65fee7f6eb3ed38ea8efe4a563d014b7ed
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-04 23:40:47 +0200

    Pagination Component created

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,9 +1,12 @@
+import { CustomPagination } from '@/components/custom/CustomPagination';
 import { CustomJumbotron } from '../../components/CustomJumbotron';
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title={'Todos los productos'} />
+
+      <CustomPagination totalPages={7} />
     </>
   );
 };
