84de6e97d5ebfd3f3d2d0efa39dc6cde906cb0f3
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-05 00:02:25 +0200

    Grid Component, filter Component and Card Component created

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,14 @@
 import { CustomPagination } from '@/components/custom/CustomPagination';
 import { CustomJumbotron } from '../../components/CustomJumbotron';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+import { products } from '@/mocks/products.mock';
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title={'Todos los productos'} />
+
+      <ProductsGrid products={products} />
 
       <CustomPagination totalPages={7} />
     </>
