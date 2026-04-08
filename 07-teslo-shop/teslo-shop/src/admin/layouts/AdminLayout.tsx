c1568d261d3702d7e61d29981f7b7e55f8e8e35e
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-08 21:22:54 +0200

    Set a few css changes inside AdminLayout

--- 07-teslo-shop/teslo-shop/src/admin/layouts/AdminLayout.tsx
+++ 07-teslo-shop/teslo-shop/src/admin/layouts/AdminLayout.tsx
@@ -7,7 +7,7 @@ const AdminLayout = () => {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
 
   return (
-    <div className="bg-gray-50 flex">
+    <div className="min-h-screen bg-gray-50 flex">
       <AdminSidebar
         isCollapsed={sidebarCollapsed}
         onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
