b1a9856ccc9e4dc1e1a55fed4f03ac7865b41951
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-08 21:10:53 +0200

    AdminLayout set with template data

--- 07-teslo-shop/teslo-shop/src/admin/layouts/AdminLayout.tsx
+++ 07-teslo-shop/teslo-shop/src/admin/layouts/AdminLayout.tsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import { AdminSidebar } from '../components/AdminSidebar';
+import { AdminHeader } from '../components/AdminHeader';
+import { Outlet } from 'react-router';
+
 const AdminLayout = () => {
-  return <div>AdminLayout</div>;
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  return (
+    <div className="bg-gray-50 flex">
+      <AdminSidebar
+        isCollapsed={sidebarCollapsed}
+        onToggle={() => setSidebarCollapsed(!sidebarCollapsed)}
+      />
+
+      <div className="flex-1 flex flex-col">
+        <AdminHeader />
+
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
