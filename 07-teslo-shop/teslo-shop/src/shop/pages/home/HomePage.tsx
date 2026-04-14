de4cfd67569f62a39c482aa8c30af60c877fb655
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-04-14 12:46:31 +0200

    CustomHook created for getProducts

--- 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
+++ 07-teslo-shop/teslo-shop/src/shop/pages/home/HomePage.tsx
@@ -2,8 +2,12 @@ import { CustomPagination } from '@/components/custom/CustomPagination';
 import { CustomJumbotron } from '../../components/CustomJumbotron';
 import { ProductsGrid } from '@/shop/components/ProductsGrid';
 import { products } from '@/mocks/products.mock';
+import { useProducts } from '@/shop/hooks/useProducts';
 
 export const HomePage = () => {
+  const { data } = useProducts();
+
+  console.log(data);
   return (
     <>
       <CustomJumbotron title={'Todos los productos'} />
