c3a97d312b5d2b98d222e216be21cde4bc3664fa
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-05-01 22:01:03 +0200

    Auth login action working in backend and frontend

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { appRouter } from './app.router';
+import { Toaster } from 'sonner';
 
 const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={appRouter} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
--- 07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
+++ 07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
@@ -3,14 +3,42 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { CustomLogo } from '@/components/custom/CustomLogo';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import { loginAction } from '@/auth/actions/login.action';
+import { toast } from 'sonner';
+import { useState } from 'react';
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+  //const { login } = useAuthStore();
+
+  const [isPosting, setIsPosting] = useState(false);
+
+  const handleLogin = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPosting(true);
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    try {
+      const data = await loginAction(email, password);
+      localStorage.setItem('token', data.token);
+      navigate('/');
+    } catch (error) {
+      toast.error('Correo o contraseña no válidos');
+    }
+
+    setIsPosting(false);
+  };
+
   return (
     <div className={'flex flex-col gap-6'}>
       <Card className="overflow-hidden p-0  ">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={handleLogin}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <CustomLogo />
@@ -24,6 +52,7 @@ export const LoginPage = () => {
                 <Input
                   id="email"
                   type="email"
+                  name="email"
                   placeholder="mail@google.com"
                   required
                 />
@@ -41,11 +70,12 @@ export const LoginPage = () => {
                 <Input
                   id="password"
                   type="password"
+                  name="password"
                   required
                   placeholder="Contraseña"
                 />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Ingresar
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
