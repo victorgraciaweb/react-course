f4198e908c28ac614860e19fb02a64e468b876a6
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-05-02 20:12:08 +0200

    Added logout button in AdminSidebar

--- 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
+++ 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Search, Bell, MessageSquare, Settings } from 'lucide-react';
+import { useAuthStore } from '@/auth/store/auth.store';
+import { Button } from '@/components/ui/button';
 
 export const AdminHeader: React.FC = () => {
+  const { user, logout } = useAuthStore();
+
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-4 h-18">
       <div className="flex items-center justify-between">
@@ -35,9 +39,12 @@ export const AdminHeader: React.FC = () => {
             <Settings size={20} />
           </button>
 
-          <div className="w-8 h-8 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center text-white font-semibold text-sm cursor-pointer hover:shadow-lg transition-shadow">
-            JD
-          </div>
+          <Button
+            onClick={logout}
+            className="w-8 h-8 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center text-white font-semibold text-sm cursor-pointer hover:shadow-lg transition-shadow"
+          >
+            {user?.fullName.substring(0, 2)}
+          </Button>
         </div>
       </div>
     </header>
