050198c4dc40336fe344b8a49f0144a9a1871b22
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-05-09 10:41:26 -0300

    Search admin function working

--- 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
+++ 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Search, Bell, MessageSquare, Settings } from 'lucide-react';
 import { useAuthStore } from '@/auth/store/auth.store';
 import { Button } from '@/components/ui/button';
+import { useSearchParams } from 'react-router';
+import { Input } from '@/components/ui/input';
 
 export const AdminHeader: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const { user, logout } = useAuthStore();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get('query') || '';
+
+  const handleSearch = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') return;
+    const query = inputRef.current?.value;
+
+    const newSearchParams = new URLSearchParams();
+
+    if (!query) {
+      newSearchParams.delete('query');
+    } else {
+      newSearchParams.set('query', inputRef.current!.value);
+    }
+
+    setSearchParams(newSearchParams);
+  };
 
   return (
     <header className="bg-white border-b border-gray-200 px-6 py-4 h-18">
@@ -16,10 +37,13 @@ export const AdminHeader: React.FC = () => {
               className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
               size={20}
             />
-            <input
+            <Input
               type="text"
+              ref={inputRef}
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
+              onKeyDown={handleSearch}
+              defaultValue={query}
             />
           </div>
         </div>
