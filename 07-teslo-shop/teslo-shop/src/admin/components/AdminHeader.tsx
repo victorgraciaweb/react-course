af7426e1ee3534fbd9abf2c7d3662e8cf5d28abe
Víctor Gracia Magallón <info@victorgraciaweb.com>
2026-05-13 16:52:55 -0300

    Removed default on input search value

--- 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
+++ 07-teslo-shop/teslo-shop/src/admin/components/AdminHeader.tsx
@@ -43,7 +43,6 @@ export const AdminHeader: React.FC = () => {
               placeholder="Search..."
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all"
               onKeyDown={handleSearch}
-              defaultValue={query}
             />
           </div>
         </div>
